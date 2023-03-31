951a18fef60e59c546a8413ae4a18b33c31f07a5
Davy Woolley <davywoolley@gmail.com>
2023-03-31 17:29:09 -0300

    Update App.js

--- trab-mobile/App.js
+++ trab-mobile/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+
+  function CaixaDeTextoComMensagem() {
+    const [texto, setTexto] = useState('');
+  
+    function handleChange(event) {
+      setTexto(event.target.value);
+    }
+  
+    return (
+      <div>
+        <input type="text" value={texto} onChange={handleChange} />
+        {texto === 'Ligado' && <p>A caixa está ligada!</p>}
+      </div>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
