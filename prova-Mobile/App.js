6612b80e05d2b46ec3a806b6b7a9d5b2d8f803fc
Davy Woolley <davywoolley@gmail.com>
2023-04-16 12:35:46 -0300

    Trabalho-quase pronto

--- prova-Mobile/App.js
+++ prova-Mobile/App.js
@@ -1,79 +1,76 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Text } from "react-native";
-import { Ionicons, AntDesign } from "@expo/vector-icons";
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
+const App = () => {
+  const [suggestions, setSuggestions] = useState([]);
+  const [inputText, setInputText] = useState('');
 
-  // Função de login
-  const handleLogin = () => {
-    // Lógica de validação de login aqui
-
-    // Simulando sucesso no login
-    setLoggedIn(true);
+  const handleInputChange = (text) => {
+    setInputText(text);
   };
+
+  const handleAddSuggestion = () => {
+    if (inputText !== '') {
+      setSuggestions([
+        ...suggestions,
+        { id: new Date().getTime().toString(), content: inputText },
+      ]);
+      setInputText('');
+    }
+  };
+
+  const handleRemoveSuggestion = (id) => {
+    setSuggestions(suggestions.filter((suggestion) => suggestion.id !== id));
+  };
+
+  const renderSuggestionItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.suggestionItem}
+      onPress={() => handleRemoveSuggestion(item.id)}
+    >
+      <Ionicons name="trash-bin" size={24} color="red" />
+      <View style={styles.suggestionItemContent}>
+        <Text style={styles.suggestionItemText}>{item.content}</Text>
+        <View style={styles.suggestionItemInfo}>
+          <Text style={styles.suggestionItemInfoText}>Por: Nome do Usuário</Text>
+          <Text style={styles.suggestionItemInfoText}>Data: DD/MM/AAAA</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      {!loggedIn ? (
-        <>
-          <Text>FUTSTATS</Text>
-
-          <Ionicons
-            name="football"
-            size={100}
-            style={styles.logo_fut}
-            color="white"
-          />
-
-          <TextInput
-            style={styles.input}
-            placeholder="Nome de usuário"
-            onChangeText={setUsername}
-            value={username}
-          />
-
-          <TextInput
-            style={styles.input}
-            placeholder="Senha"
-            secureTextEntry
-            onChangeText={setPassword}
-            value={password}
-          />
-          <Button title="Login" onPress={handleLogin} />
-        </>
-      ) : (
-        <View style={styles.loggedInContainer}>
-          <Button title="Logout" onPress={() => setLoggedIn(false)} />
-          <Text>FUTSTATS</Text>
-
-          <View style={styles.playerContainer}>
-            <View style={styles.playerBox}>
-              <Text>HAALAND</Text>
-            </View>
-            <View style={styles.playerBox}>
-              <Text>MBAPPE</Text>
-            </View>
-          </View>
-
-          <View style={styles.statsContainer}>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-            <View style={styles.statsBox}><Text>aaaa</Text></View>
-          </View>
-        </View>
-      )}
+      <View style={styles.header}>
+        <Text style={styles.title}>Sugestões para o Clube</Text>
+        <Ionicons name="add-circle" size={28} color="#2e88ff" onPress={handleAddSuggestion} />
+      </View>
+      <ScrollView style={styles.suggestionsList}>
+        <FlatList
+          data={suggestions}
+          renderItem={renderSuggestionItem}
+          keyExtractor={(item) => item.id}
+        />
+      </ScrollView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleInputChange}
+          value={inputText}
+          placeholder="Digite sua sugestão"
+        />
+        <Ionicons name="send" size={24} color="#2e88ff" onPress={handleAddSuggestion} />
+      </View>
     </View>
   );
 };
@@ -81,60 +78,64 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 16,
-    backgroundColor: "#063508",
+    backgroundColor: '#fff',
   },
-  logo_fut: {
-    paddingBottom: 100,
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
   },
   input: {
-    width: "100%",
+    flex: 1,
     height: 40,
+    borderColor: '#ccc',
     borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 4,
-    paddingLeft: 40,
-    marginBottom: 16,
-    backgroundColor: "#85DE5C",
+    borderRadius: 20,
+    paddingLeft: 16,
+    paddingRight: 16,
+    marginRight: 8,
   },
-  loggedInContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  
-  playerContainer: {
+  suggestionsList: {
     flex: 1,
-    flexDirection: "row",
-    margin: 10,
   },
-  playerBox: {
-    marginHorizontal: 30,
-    backgroundColor: "#85DE5C",
-    height: 110,
-    width: 140,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  statsContainer: {
+  suggestionItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingBottom: 8,
+    marginBottom: 8,
+    },
+    suggestionItemContent: {
     flex: 1,
-    flexDirection: "row",
-    margin: 10,
-  },
-  statsBox: {
-    marginHorizontal: 30,
-    backgroundColor: "#85DE5C",
-    height: 110,
-    width: 140,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 10
-  },
-});
-
-export default Login;
+    marginLeft: 16,
+    },
+    suggestionItemText: {
+    fontSize: 16,
+    color: '#333',
+    },
+    suggestionItemInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 4,
+    },
+    suggestionItemInfoText: {
+    fontSize: 12,
+    color: '#666',
+    },
+    });
+    
+    export default App;
